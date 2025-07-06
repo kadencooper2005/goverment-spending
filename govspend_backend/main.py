483b9ce491b0d911eb33741f515af0956ec717b1
Slowrabbets <kadencooper608@gmail.com>
2025-07-06 13:13:19 -0600

    Add .env file

--- govspend_backend/main.py
+++ govspend_backend/main.py
@@ -1,13 +1,19 @@
 from fastapi import FastAPI
 from govspend_backend.routers import spending
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
+URL = os.getenv("URL")
 
 ## Entry point for the API
 app = FastAPI(title="GovSpend Intel API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[URL],
     allow_credentials=True,
     allow_methods=["GET"],
     allow_headers=["*"],
