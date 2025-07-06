be6429efef907cca7f33ee881445426935f1fb1c
Slowrabbets <kadencooper608@gmail.com>
2025-07-06 13:18:44 -0600

    Changed URL for backend

--- govspend_frontend/src/lib/api.ts
+++ govspend_frontend/src/lib/api.ts
@@ -1,5 +1,5 @@
 export async function fetchSpendingData(year: number, quarter: number, agency: string) {
-    const res = await fetch(`http://localhost:8000/spending/?year=${year}&quarter=${quarter}&agency=${agency}`);
+    const res = await fetch(`https://goverment-spending.onrender.com/spending/?year=${year}&quarter=${quarter}&agency=${agency}`);
     if (!res.ok) {
         throw new Error("Failed to fetch spending data");
     }
